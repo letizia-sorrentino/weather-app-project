80313c23341a96ef881ea4c5478422965a7e7530
Letizia Sorrentino <letizia.sorrentino@gmail.com>
2023-05-10 11:29:12 +0100

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 //DOM elements
-const form = document.getElementById('city');
-const input = document.getElementById('cityInput');
-const button= document.getElementById('btn');
+const search = document.getElementById('search');
 const msg = document.querySelector('.form-message');
 const cityName = document.getElementById('city-name');
 const todayCard = document.getElementById('today-card');
 const hourlyCard = document.getElementById('hourly-card');
 const forecastCard = document.getElementById('forecast-card');
 
-
-form.addEventListener('submit', e => {
-    //prevent default form submission
-    e.preventDefault();
-    
-    //hide any message that might be displayed
-    msg.textContent = '';
-    msg.classList.remove('visible');
-    
-    //get the search value
-    let inputVal = input.value;
-    console.log(inputVal);
-    return inputVal;
-    });
-
+//Variables
+let searchValue;
+let apiData;
 
 //Async function working with Geolocation
 const success = async ({ coords }) => {
@@ -35,21 +21,21 @@ const success = async ({ coords }) => {
     console.log(data);
     console.log(data.city.name);
 
-   //getLocation(data);
+    //getLocation(data);
     setWeather(data, data.list);
 }
 
 const setWeather = (data, list) => {
     //Show City Name 
-        const city = data.city.name;
-        const location = `<div class="location"><h2>${city}</h2></div> ` 
+    const city = data.city.name;
+    const location = `<div class="location"><h2>${city}</h2></div> `
 
     //today-weather card
-    const todayData = list.slice(0,1);
+    const todayData = list.slice(0, 1);
     const todayWeather = todayData.map(element => {
         return `<div class="today-weather"> 
             <div class="today-weather-icon"><img src="https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png"></div>
-            <div class="today-weather-info"><p class="temp">${Math.round(element.main.temp- 273.15)}&#8451</p>
+            <div class="today-weather-info"><p class="temp">${Math.round(element.main.temp - 273.15)}&#8451</p>
             <p class="temp-max">H. ${Math.round(element.main.temp_max - 273.15)}&#8451</p>
             <p class="temp-min">L. ${Math.round(element.main.temp_min - 273.15)}&#8451</p>
             <p class="description">${element.weather[0].description}</p>
@@ -71,14 +57,14 @@ const setWeather = (data, list) => {
     });
 
     //daily Forecast Card
-    const forecastData = list.slice(4,7);
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    
+    const forecastData = list.slice(4, 7);
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     const forecastWeather = forecastData.map(element => {
         return `<div class="forecast-weather" >
         <div class="weather-icon"><img src="https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png"></div>
         <div class="weather-info> 
-        <p class="day">${weekday[new Date(element.dt*1000).getDay()]}</p>
+        <p class="day">${weekday[new Date(element.dt * 1000).getDay()]}</p>
         <p class="temp">${Math.round(element.main.temp - 273.15)}&#8451</p>
         <p class="description">${element.weather[0].description}</p>
         </div >
@@ -99,7 +85,33 @@ const error = (error) => {
 const config = {
     enableHighAccuracy: true,
     maximumAge: 0,
-    timeout: 1000
+    timeout: 500
 }
 
 navigator.geolocation.getCurrentPosition(success, error, config);
+
+const updateInterface = () => {
+    //Show City Name 
+    cityName.innerHTML = `<h2>${apiData.city.name}</h2>`
+};
+
+const getData = async () => {
+
+    try {
+        const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=7191fefc1ad22b3e9a87628b612c82a9`);
+        apiData = data;
+        console.log(apiData);
+        updateInterface();
+    } catch (error) {
+        console.log('Api said NO!')
+    }
+}
+
+//User's input 
+search.addEventListener('input', e => {
+
+    //get the search value
+    searchValue = e.target.value;
+    console.log(searchValue);
+    getData();
+});
